fd1d4480a35d63f9b74eca945505004944ca6762
Saksham Saini <sakshamsaini18@gmail.com>
2025-10-09 11:07:29 -0400

    delete env

--- config/environments/production.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.configure do
-    # Existing production config...
-    # Disable force SSL for public IP testing
-    config.force_ssl = false
-    config.middleware.delete ActionDispatch::SSL
-end
