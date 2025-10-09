37451503b5b351de39638e4a076dfcdb48f5862f
Saksham Saini <sakshamsaini18@gmail.com>
2025-10-09 16:46:05 -0400

    remove bind

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,6 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-# Bind to all interfaces on port 3000 (plain HTTP)
-bind "tcp://0.0.0.0:3000"
 
 environment ENV.fetch("RAILS_ENV") { "production" }
 
