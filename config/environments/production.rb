fa78b1b93d05ab3f8c98de8292c6483bb22e4e5d
Saksham Saini <sakshamsaini18@gmail.com>
2025-10-09 11:04:43 -0400

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,1 +1,6 @@
-config.force_ssl = false
+Rails.application.configure do
+    # Existing production config...
+    # Disable force SSL for public IP testing
+    config.force_ssl = false
+    config.middleware.delete ActionDispatch::SSL
+end
